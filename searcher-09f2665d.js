e2aea17528acbe03c513a2b4d605493b393b312e
siccegge <siccegge@users.noreply.github.com>
2026-07-18 15:05:30 +0000

    deploy: 85ff698ad33eca2f064b61f630b87ca75efee001

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-c140e876.js',
+                path_to_root + 'searchindex-a950e615.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
